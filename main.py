correct reward calculation

main.py
@@ -91,20 +91,20 @@ def main(args):
         # np.save('y_test.npy',y_test)
         env = Game(cfg = cfg, dcrnn = temp_dcrnn,action_space = beta_epsilon_all, scenario_dict = scenario_dict, dataset_idx = 0)
         dqn = DQN(config, cfg, env, mask_init)
-        for i in range(300):
+        for i in range(len(scenario_dict["context_pts"]), -1, -1):
             env = Game(cfg = cfg, dcrnn = temp_dcrnn,action_space = beta_epsilon_all, scenario_dict = scenario_dict, dataset_idx = i)
             dqn.env = env
             train_DQN(dqn, temp_dcrnn, beta_epsilon_all, scenarios = scenario_dict, config=config, cfg=cfg, episodes = cfg.TRAIN.episode)
-        checkpoint_path = "results/dqn_ckpt_{i}.pth".format(i)
-        if(i%20):
-            torch.save({
-                'model': dqn.q_network_local.state_dict(),
-                'optimizer': dqn.q_network_optimizer.state_dict(),
-                #'epoch': epoch,
-                'config': cfg,
-                #'loss': loss_list,
-                #'val_mae': val_mae_list
-            }, checkpoint_path)
+            checkpoint_path = "results/dqn_ckpt_{}.pth".format(i)
+            if(i%20):
+                torch.save({
+                    'model': dqn.q_network_local.state_dict(),
+                    'optimizer': dqn.q_network_optimizer.state_dict(),
+                    #'epoch': epoch,
+                    'config': cfg,
+                    #'loss': loss_list,
+                    #'val_mae': val_mae_list
+                }, checkpoint_path)
         exit()
 
 
@@ -239,7 +239,7 @@ def build_config_dict(cfg, num_actions):
     config.file_to_save_data_results = "results/"
     config.file_to_save_results_graph = "results/"
     config.show_solution_score = False
-    config.visualise_individual_results = False
+    config.visualise_individual_results = True
     config.visualise_overall_agent_results = True
     config.standard_deviation_results = 1.0
     config.runs_per_agent = 1

utils/agents/Base_Agent.py
@@ -16,12 +16,14 @@ class Base_Agent(object):
         # if self.debug_mode: self.tensorboard = SummaryWriter()
         self.SAVE_PATH = cfg.DIR.output_dir
         self.config = config
+        self.cfg = cfg
         self.set_random_seeds(config.seed)
         self.environment = env
         self.action_mask = action_mask
         self.environment_title = "STNP_reward_env"
         self.action_types = "DISCRETE"
         self.action_size = config.action_size
+        self.reward_penalty = cfg.TRAIN.reward_penalty
         self.lowest_possible_episode_score = self.get_lowest_possible_episode_score()
 
         self.state_size =  config.state_size
@@ -143,9 +145,30 @@ class Base_Agent(object):
 
     def conduct_action(self, action):
         """Conducts an action in the environment"""
-        self.next_state, self.reward, self.done, _ = self.environment.step(action)
+        self.next_state, self.reward, self.done, info = self.environment.step(action)
         self.total_episode_score_so_far += self.reward
         self.mask[action] = 1
+        self.action_idx_ls.append(self.action)
+        if(self.done):
+            reward_arr = info["gt_reward_arr"].numpy()
+            sorted_ind = np.argsort(reward_arr.copy()) #get indices of top rewards, argsort is in ascending order
+            sorted_rewards = reward_arr[sorted_ind]
+            weights = self.reward_penalty**(np.arange(9, -1, -1))
+            weighted_rewards= sorted_rewards[-10:]*weights
+            top_twenty_union = np.intersect1d(self.action_idx_ls, sorted_ind[-20:])
+            top_ten_union = np.intersect1d(self.action_idx_ls, sorted_ind[-10:])
+            
+            
+            self.action_idx_ls = torch.tensor(self.action_idx_ls)
+            top_chosen_rewards = np.sort(reward_arr[self.action_idx_ls])[-10:]
+            print('ACTION LIST - DQN = {}'.format(self.action_idx_ls))
+            print('GT TOP 20 IDX = {}'.format(sorted_ind[-20:]))
+            print('DQN - top 10 chosen rewards = ', top_chosen_rewards)
+            print('GT - top 10 rewards = {},'.format(sorted_rewards[-10:]))
+            print('DQN - top_twenty_union = {}, top_ten_union = {}'.format(len(top_twenty_union),len(top_ten_union))) 
+            print('OVERALL - gt_weighted reward = {}, chosen sum = '.format(sum(weighted_rewards), self.total_episode_score_so_far))
+            
+                  
         if self.hyperparameters["clip_rewards"]: self.reward =  max(min(self.reward, 1.0), -1.0)
         return self.next_state, self.reward, self.done
 

utils/agents/DQN.py
@@ -21,11 +21,15 @@ class DQN(Base_Agent):
         self.q_network_optimizer = optim.Adam(self.q_network_local.parameters(),
                                               lr=self.hyperparameters["learning_rate"], eps=1e-4)
         self.exploration_strategy = Epsilon_Greedy_Exploration(config)
-        self.mask = action_mask.reshape(-1,1)
+        self.mask_init = action_mask.reshape(-1,1)
+        self.mask = self.mask_init.copy()
+        self.action_idx_ls = []
 
     def reset_game(self):
         super(DQN, self).reset_game()
         self.update_learning_rate(self.hyperparameters["learning_rate"], self.q_network_optimizer)
+        self.mask = self.mask_init.copy()
+        self.action_idx_ls = []
 
     def step(self):
         """Runs a step within a game including a learning step if required"""
@@ -63,8 +67,6 @@ class DQN(Base_Agent):
             action = np.random.randint(0, 270)
         else:
             action = torch.argmax(action_values).item()
-        if(action>=270):
-            import ipdb;ipdb.set_trace()
         self.logger.info("Q values {} -- Action chosen {}".format(action_values, action))
         return action
 

utils/env/environment.py
@@ -15,7 +15,7 @@ class Game():
         self.device = torch.device(cfg.TRAIN.device)
         self.action_space = action_space
         self.scenario_dict = scenario_dict
-        self.action_made = 0
+        self.reward_penalty = cfg.TRAIN.reward_penalty
         self.MAX_ACTIONS  = cfg.TRAIN.max_actions
         self.cfg = cfg
         self.dcrnn = dcrnn
@@ -45,28 +45,21 @@ class Game():
             x_train = torch.cat([x_c, x_t], dim = 1)[0]
             y_train = torch.cat([y_c, y_t], dim = 1)[0]
             self.gt_reward_arr = self.calculate_score(self.cfg, self.dcrnn, x_train, y_train, self.action_space)
-        if(self.action_made == 1):
-            print('max action = {}'.format(self.MAX_ACTIONS))
-            torch.sort(self.gt_reward_arr)
-            print("reward stats: mean = {}, median = {}, max = {}, top 5 values = {}".format(
-                torch.mean(self.gt_reward_arr), torch.median(self.gt_reward_arr), torch.max(self.gt_reward_arr),  self.gt_reward_arr[-5:]
-            ))
+        if(self.actions_made == 0):
+            print("reward stats: mean = {}, median = {}, max = {}".format(
+                torch.mean(self.gt_reward_arr), torch.median(self.gt_reward_arr), torch.max(self.gt_reward_arr)))
         
         selected_param = self.action_space[action_idx]
         #observation = self.scenario_dict
-        ct_shape = torch.tensor(self.context_pts.shape[-2:])
-        tgt_shape = torch.tensor(self.target_pts.shape[-2:])
-        observation = torch.cat([self.context_pts.flatten(), self.target_pts.flatten(), self.latent_variable.flatten(),ct_shape , tgt_shape], dim = 0)
-        reward = self.gt_reward_arr[action_idx]
-        self.action_made +=1
-        #done = {"__all__": self.action_made >= self.MAX_ACTIONS}
-        done = self.action_made >= self.MAX_ACTIONS
+        observation = self.dict_to_state(self.context_pts, self.target_pts, self.latent_variable) #convert from dictionary to vector(RL state has to be a flattened vector)
+        reward = self.gt_reward_arr[action_idx]*self.reward_penalty**(self.actions_made)
+        self.actions_made +=1
+        done = self.actions_made >= self.MAX_ACTIONS
         print('data_num = {}, actions_made = {}, action = {}, reward = {}'.format(self.dataset_idx, self.actions_made, action_idx, reward))
         print('done = ', done)
-        info = {"selected_param": selected_param, "gt_reward_arr": self.gt_reward_arr}
+        info = {"selected_param": selected_param, "gt_reward_arr": self.gt_reward_arr.flatten()}
         
         return observation, reward, done, info
-
 
     def get_reward(self, action_idx):
         return self.gt_rewards[action_idx]
@@ -114,23 +107,27 @@ class Game():
         return score_array
 
     def reset(self):
-        idx = self.dataset_idx
+        idx = self.dataset_idx +1
         self.context_pts = self.scenario_dict["context_pts"][idx:idx+1]
         self.target_pts = self.scenario_dict["target_pts"][idx:idx+1]
         self.latent_variable = self.scenario_dict["latent_variable"][idx:idx+1]
         self.valid_pred = self.scenario_dict["valid_pred"][idx:idx+1]
         self.test_pred = self.scenario_dict["test_pred"][idx:idx+1]
         
-        ct_shape = torch.tensor(self.context_pts.shape[-2:])
-        tgt_shape = torch.tensor(self.target_pts.shape[-2:])
-        observation = torch.cat([self.context_pts.flatten(), self.target_pts.flatten(), self.latent_variable.flatten(),ct_shape, tgt_shape], dim = 0)
+        observation = self.dict_to_state(self.context_pts, self.target_pts, self.latent_variable)
+        self.actions_made = 0
         print('reset game - observation shape = {}'.format(observation.shape))
-        
-        
-        self.action_made = 0
-        
-        
         return observation
+    
+    def dict_to_state(self, context_pts, target_pts, latent_variable):
+        ct_shape = torch.tensor(context_pts.shape[-2:])
+        tgt_shape = torch.tensor(target_pts.shape[-2:])
+        context_pts = context_pts.flatten()
+        target_pts = target_pts.flatten()
+        latent_variable = latent_variable.flatten()
+        observation = torch.cat([context_pts,target_pts , latent_variable, ct_shape, tgt_shape], dim = 0)
+        return observation
+        
     
     def render(self, mode):
         pass